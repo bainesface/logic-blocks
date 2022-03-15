acb2071135fec8d13b812a039073811c1fa75e22
bainesface <bainesface@gmail.com>
2022-03-15 15:55:43 +0000

    complete part two sans tests, sorreeee

--- reposeRecord/reposeRecord.js
+++ reposeRecord/reposeRecord.js
@@ -1,10 +1,45 @@
-const reposeRecord = (data) => {
+const { getData } = require("./data");
+
+exports.reposeRecord = (data) => {
 	const sortedUpdates = sortUpdates(data);
+	const record = makeRecord(sortedUpdates);
+	const sleepyGuard = findSleepiestGuard(record);
+	// return zero if none sleeping
+	if (!sleepyGuard) return 0;
+
+	// finding the most frequent sleepy slot
+	const mostFrequentTime = findSleepiestTime(record[sleepyGuard].minsAsleep);
+
+	return +sleepyGuard * mostFrequentTime;
+};
+
+exports.getGuardId = async () => {
+	const data = await getData();
+	const sortedData = sortUpdates(data);
+	const record = makeRecord(sortedData);
+	const updatedRecord = addSleepiestMins(record);
+	const sleepyGuard = getSleepyGuard(updatedRecord);
+
+	return sleepyGuard * record[sleepyGuard].sleepiestMin;
+};
+
+const sortUpdates = (data) => {
+	const guardUpdates = data.split("[");
+	guardUpdates.shift();
+	const sepUpdates = guardUpdates.map((update) => {
+		const sepDates = update.split("] ");
+		return sepDates;
+	});
+	const orderedUpdates = sepUpdates.sort((date1, date2) => {
+		return new Date(date1[0]) - new Date(date2[0]);
+	});
+	return orderedUpdates;
+};
+
+const makeRecord = (updates) => {
 	const record = {};
 	let currentGuard = 0;
-
-	// adding data to record object
-	sortedUpdates.forEach((update) => {
+	updates.forEach((update) => {
 		let guardNum = update[1].match(/\d/g);
 		if (guardNum) {
 			guardNum = guardNum.join("");
@@ -32,8 +67,10 @@ const reposeRecord = (data) => {
 			record[currentGuard].totalSleep += min - lastSleepTime;
 		}
 	});
+	return record;
+};
 
-	// getting the sleepiest guard
+const findSleepiestGuard = (record) => {
 	let mostSleep = 0;
 	let sleepyGuard;
 
@@ -43,19 +80,15 @@ const reposeRecord = (data) => {
 			sleepyGuard = guard;
 		}
 	}
+	return sleepyGuard;
+};
 
-	// return zero if no sleeping
-	if (!sleepyGuard) return 0;
-
-	// finding the most frequent sleepy slot
-	const times = record[sleepyGuard].minsAsleep.reduce(
-		(timesSoFar, currTime) => {
-			if (!timesSoFar[currTime]) timesSoFar[currTime] = 1;
-			else timesSoFar[currTime]++;
-			return timesSoFar;
-		},
-		{}
-	);
+const findSleepiestTime = (minsAsleep) => {
+	const times = minsAsleep.reduce((timesSoFar, currTime) => {
+		if (!timesSoFar[currTime]) timesSoFar[currTime] = 1;
+		else timesSoFar[currTime]++;
+		return timesSoFar;
+	}, {});
 
 	let noOfTimes = 0;
 	let mostFrequentTime = 0;
@@ -66,22 +99,43 @@ const reposeRecord = (data) => {
 			mostFrequentTime = time;
 		}
 	}
-
-	return +sleepyGuard * mostFrequentTime;
+	return mostFrequentTime;
 };
 
-// sorting updates chronologically
-const sortUpdates = (data) => {
-	const guardUpdates = data.split("[");
-	guardUpdates.shift();
-	const sepUpdates = guardUpdates.map((update) => {
-		const sepDates = update.split("] ");
-		return sepDates;
-	});
-	const orderedUpdates = sepUpdates.sort((date1, date2) => {
-		return new Date(date1[0]) - new Date(date2[0]);
-	});
-	return orderedUpdates;
+const addSleepiestMins = (updatesRecord) => {
+	const record = { ...updatesRecord };
+
+	for (let guard in record) {
+		const minsAsleep = record[guard].minsAsleep.reduce((minsTally, currMin) => {
+			if (!minsTally[currMin]) minsTally[currMin] = 1;
+			else minsTally[currMin]++;
+			return minsTally;
+		}, {});
+
+		let mostFrequentMin = 0;
+		let numTimesAsleep = 0;
+
+		for (let mins in minsAsleep) {
+			if (minsAsleep[mins] > numTimesAsleep) {
+				numTimesAsleep = minsAsleep[mins];
+				mostFrequentMin = mins;
+			}
+		}
+		record[guard].sleepiestMin = mostFrequentMin;
+		record[guard].timesAsleepAtSleepyMin = numTimesAsleep;
+	}
+	return record;
 };
 
-module.exports = { reposeRecord, sortUpdates };
+const getSleepyGuard = (record) => {
+	let sleepyGuard;
+	let numsTimesAsleep = 0;
+
+	for (let guard in record) {
+		if (record[guard].timesAsleepAtSleepyMin > numsTimesAsleep) {
+			numsTimesAsleep = record[guard].timesAsleepAtSleepyMin;
+			sleepyGuard = guard;
+		}
+	}
+	return sleepyGuard;
+};
